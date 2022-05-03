193c0f243542748616d30453addef642ece7c233
Jyoti Nolastname (Student) <jnolas15@sundevils.asu.edu>
2022-05-02 19:24:26 -0700

    Committed for distance

--- src/CirclePanel.java
+++ src/CirclePanel.java
@@ -1,14 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
 import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.awt.Graphics;
-import java.util.Scanner;
 import javax.swing.JPanel;
+import java.lang.Math;
+import java.util.List;
 
 public class CirclePanel extends JPanel {
 	private List<DrawCircle> circles = new LinkedList<DrawCircle>();
+	int visited[] = new int[100];
+	int a[] = new int[100];
+
+	int v=0;
+	int m=0;
+	boolean isdrawCircle = true;
 
 	public void addCircle(DrawCircle circle) {
 		circles.add(circle);
@@ -17,12 +24,120 @@ public class CirclePanel extends JPanel {
 
 	@Override
 	public void paint(Graphics g) {
-		for (DrawCircle c : circles) {
-			c.draw(g);
+		//super.paint(g);
+		if(isdrawCircle) {
+			for (DrawCircle c : circles) {
+				c.draw(g);
+			}
+
+			System.out.println("called paint function");
+
 		}
 
 	}
 
+	public void run() {
+		System.out.println("run called");
+		int count = 0;
+		//lineDB = new ArrayList<>();
+
+
+		try {
+			File coordinates = new File("newfile.txt");
+			Scanner scanner = new Scanner(coordinates);
+			List x= new ArrayList();
+			List y = new ArrayList();
+			int i = 0;
+			String s1 = "";
+			while (scanner.hasNext()) {
+
+				s1 = scanner.nextLine();
+				String s3 = s1.substring(0, s1.indexOf(","));
+				x.add(Integer.parseInt(s3));
+				String s2 = s1.substring(s1.indexOf(",") + 1);
+				y.add(Integer.parseInt(s2));
+				a[i]=i;
+				System.out.println();
+				i++;
+				count++;
+
+			}
+
+			int rNum=1;
+			Random rand = new Random();
+			System.out.println("Array elements>>>>>>>>>\n\n");
+			for (int j = 0; j < count; j++) {
+				System.out.println( x.get(j) + "  " + y.get(j));
+			}
+		//	Arrays.sort(unvisited);
+		//	int cond1 = Arrays.binarySearch(unvisited, 0);
+
+			for(int f=0;f<count;f++) {
+				Arrays.sort(visited);
+				Arrays.sort(a);
+				rNum = rand.nextInt(count);
+				System.out.println("\n\n---Enter your distance parameter: ");
+				Scanner sc = new Scanner(System.in);
+				float distParam = sc.nextFloat();
+				double dist;
+
+				for (int u = 0; u < count; u++) {
+					Arrays.sort(visited);
+					int cond = Arrays.binarySearch(visited, rNum);
+
+					if (cond < 0 && (rNum == u)) {
+
+						visit(rNum);
+						continue;
+					} else {
+						dist = Math.hypot((int) x.get(rNum) - (int) x.get(u), (int) y.get(rNum) - (int) y.get(u));
+						System.out.println(dist);
+						if (dist <= distParam) {
+							isdrawCircle = false;
+							paint(this.getGraphics());
+							try {
+								Thread.sleep(2000);
+							} catch (InterruptedException e) {
+								throw new RuntimeException(e);
+							}
+							repaint();
+
+
+							visit(u);
+							System.out.println("Visited array:" + Arrays.toString(visited));
+						}
+						//else{
+					//		unvisit(u);
+					//		System.out.println("Visited array:" + Arrays.toString(visited));
+						//}
+					}
+				}
+				isdrawCircle = true;
+				if(Arrays.equals(visited,a))
+					break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+	}
+	public void visit(int a)
+	{
+
+
+		visited[v]=a;
+		v++;
+	}
+	/*
+	public void unvisit(int a)
+	{
+
+
+		unvisited[m]=a;
+		m++;
+	}
+
+	 */
 	public void save() {
 		FileOutputStream fop = null;
 		File file;
@@ -30,7 +145,7 @@ public class CirclePanel extends JPanel {
 
 		try {
 
-			file = new File("C:\\Users\\jappaka\\Desktop\\newfile.txt");
+			file = new File("newfile.txt");
 			fop = new FileOutputStream(file);
 
 			// if file doesnt exists, then create it
@@ -74,17 +189,12 @@ public class CirclePanel extends JPanel {
 				System.out.println("Selected file: " + selectedFile.getAbsolutePath());
 				fi = new FileInputStream(new File(selectedFile.getAbsolutePath()));
 
-//ObjectInputStream oi = new ObjectInputStream(fi);
 				Scanner sc = new Scanner(fi);
 				System.out.println(fi);
 				DrawCircle obj = null;
 				circles = new LinkedList<DrawCircle>();
 				System.out.println(circles);
-//while ((obj = fi.read()) != null) {
-// obj = (Circle) oi.readObject();
-// System.out.println(obj);
-// circles.add(obj);
-//}
+
 
 				while (sc.hasNextLine()) {
 					String[] point = sc.nextLine().split(",");
@@ -103,4 +213,5 @@ public class CirclePanel extends JPanel {
 			this.repaint();
 		}
 	}
+
 }
